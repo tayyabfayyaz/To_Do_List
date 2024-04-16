9f092f7e4aca18dbac25eda058fcd3162419f306
Your Name <fayyaztayyab684@gmail.com>
2024-04-16 13:29:20 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,26 @@
+import inquirer from 'inquirer';
+
+let to_do = [];
+let i = 50;
+
+while (i = 50) {
+    let userInput = await inquirer.prompt(
+        [
+            {
+                name: "todo",
+                type: 'input',
+                message: "What you want to add in To-Do List?",
+            },
+    
+            {
+                name: "sure",
+                type: 'confirm',
+                message: "Are you sure add new element in your list.",
+                default: false,
+            }
+        ]
+    )
+    to_do.push(userInput.todo);
+    console.log(to_do);
+    i++;
+}
