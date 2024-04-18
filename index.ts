f2aa03abaac5d746a56035754bc1ea2b69b72640
Your Name <fayyaztayyab684@gmail.com>
2024-04-19 03:49:33 +0500

    File update

--- index.ts
+++ index.ts
@@ -1,26 +1,44 @@
+#! /usr/bin/env node
+
 import inquirer from 'inquirer';
 
 let to_do = [];
-let i = 50;
+let condition = true;
+let removeCondition = true;
 
-while (i = 50) {
-    let userInput = await inquirer.prompt(
+while (condition) {
+    let todoQuestions = await inquirer.prompt(
         [
             {
-                name: "todo",
+                name: "first",
                 type: 'input',
                 message: "What you want to add in To-Do List?",
             },
     
             {
-                name: "sure",
+                name: "second",
                 type: 'confirm',
-                message: "Are you sure add new element in your list.",
-                default: false,
-            }
+                message: "Are you sure add new To-Do in your list.",
+                default: true,
+            },
         ]
     )
-    to_do.push(userInput.todo);
-    console.log(to_do);
-    i++;
+        to_do.push(todoQuestions.first);
+        console.log(to_do);
+        condition = todoQuestions.second;
+}
+
+while (removeCondition) {
+   let removeConditionTrue = await inquirer.prompt(
+    {
+        name:"falseCondition",
+        type: 'confirm',
+        message: "Do you want to remove TO-Do List last element? ",
+        default: false,
+    }
+   )
+   removeCondition = removeConditionTrue.falseCondition;
+   to_do.pop();
+   console.log(to_do);
+
 }
